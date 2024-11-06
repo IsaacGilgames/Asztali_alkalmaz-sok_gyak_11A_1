6ff745fe2f1d10a8afd21991d4c93f517e981365
Martin <isaacmartin115@gmail.com>
2024-11-06 11:42:53 +0100

    1106

---
+++ HaziFeladatEllenorzes/HaziFeladatEllenorzes/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaziFeladatEllenorzes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("1-től 100-ig a számok, amelyek nem oszthatók 8-cal, de 2-vel igen: ");
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0 && i % 8 != 0)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+            Console.WriteLine("Adj meg egy számot!");
+            int num = int.Parse(Console.ReadLine());
+            int x = 1;
+            for (int i = num; i > 1; i--)
+            {   if(i-1 != 1)
+                {
+                    x *= i;
+                    Console.WriteLine($"{num}! = {x} * {i - 1}!");
+                }
+                else
+                {
+                    x *= i;
+                    Console.WriteLine($"{num}! = {x}");
+                }
+               
+            }
+
+            int honap = int.Parse(Console.ReadLine());
+            switch (honap)
+            {
+                case 1:
+                case 3:
+                    Console.WriteLine("31 napos");
+                    break;
+            }
+
+
+            //Gyakorlas1104
+            //100-90 5
+            //89-70 4
+            //69-50 3
+            //49-30 2
+            //29-0 1
+
+            //Kérd be a dolgozat hány százalékos lett (kerekítve)
+            //és írd ki az eredmény!
+
+
+            int gondoltam = 42;
+            //Készíts egy programot, amely addig kér be számot
+            //ameddig a gondolt számot nem írod be!
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ fuggvenyek1/fuggvenyek1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuggvenyek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Szám: ");
+            int szam = int.Parse(Console.ReadLine());
+            Console.WriteLine(Faktorialis(szam));
+
+            Console.Write("\n\n\nKör sugara: ");
+            double r = 0;
+            beker(ref r);
+            Console.WriteLine("A kör kerülete: {0} és területe: {1}", Kerulet(r),Terulet(r));
+            //Console.WriteLine(r);
+
+            Console.Write("Hány sort szeretnél a háromszögben: ");
+            int sor = int.Parse(Console.ReadLine());
+            HaromszogRajz(sor);
+            //Önállóan
+            /*
+             
+
+             * 
+             **
+             ***
+             ****
+             *****
+             
+
+             */
+            Console.ReadKey();
+
+        }
+
+        static int Faktorialis(int sz)
+        {
+            int szorzat = 1;
+            for (int i = 2; i <= sz; i++)
+            {
+                szorzat *= i;
+            }
+            return szorzat;
+        }
+        //bekérés
+        static void beker(ref double r)
+        {
+            r = double.Parse(Console.ReadLine());
+        }
+
+        //kerület számítás
+        static double Kerulet(double r)
+        {
+            double k = 2 * r * Math.PI;
+            return k;
+        }
+        //terület számítás
+        static double Terulet(double r)
+        {
+            double t = Math.Pow(r, 2) * Math.PI;
+            return t;
+        }
+
+
+        
+
+    }
+}
